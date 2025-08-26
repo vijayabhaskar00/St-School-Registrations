aa1a45c9cd5904be637cce3797762097d257e62a
javed <javed1709@gmail.com>
2025-08-27 01:41:25 +0530

    changes

--- setAdmin.js
+++ setAdmin.js
@@ -1,7 +1,14 @@
 import admin from 'firebase-admin'
 import { readFileSync } from 'fs'
 
-const serviceAccount = JSON.parse(readFileSync('./serviceAccountKey.json', 'utf8'))
+let serviceAccount
+if (process.env.SERVICE_ACCOUNT_KEY) {
+  // Load from environment variable (e.g. in Vercel)
+  serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY)
+} else {
+  // Fallback to local file for development
+  serviceAccount = JSON.parse(readFileSync('./serviceAccountKey.json', 'utf8'))
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
